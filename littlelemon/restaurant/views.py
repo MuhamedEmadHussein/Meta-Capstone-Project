ab086818ca96d549f06bef53b3ae4f679e677b85
MuhammadEmadHussein <mohamedemadhu01019@gmail.com>
2024-02-22 13:26:05 +0200

    Capstone Functionality finished

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -5,7 +5,7 @@ from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-# from .forms import BookingForm
+from .forms import BookingForm
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.core import serializers
@@ -46,3 +46,46 @@ class BookingViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = bookingSerializer            
+
+def book(request):
+    form = BookingForm
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, 'book.html', context)
+
+def about(request):
+    return render(request, "about.html")
+
+@csrf_exempt
+def bookings(request):
+    if request.method == 'POST':
+        data = json.load(request)
+        exist = Booking.objects.filter(booking_date=data['booking_date']).filter(
+            no_of_guests=data['no_of_guests']).exists()
+        if exist==False:
+            booking = Booking(
+                name=data['name'],
+                booking_date=data['booking_date'],
+                no_of_guests=data['no_of_guests'],
+            )
+            booking.save()
+        else:
+            return HttpResponse("{'error':1}", content_type='application/json')
+    
+    date = request.GET.get('date',datetime.today().date())
+
+    bookings = Booking.objects.all().filter(booking_date=date)
+    booking_json = serializers.serialize('json', bookings)
+
+    return HttpResponse(booking_json, content_type='application/json')
+
+
+def reservations(request):
+    date = request.GET.get('date', datetime.today().date())
+    print("date: ", date)
+    bookings = Booking.objects.all().filter(booking_date = date)
+    booking_json = serializers.serialize('json', bookings)
+    return render(request, 'bookings.html', {'bookings': booking_json})
